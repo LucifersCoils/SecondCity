LucifersCoils/SecondCity

commit 2a441e0f9a8fea7c9c63e4828ddd27a77123385b

fake platforms (#310)
## About The Pull Request

**speed pr because i'm tired**

adds "platform" walls based on the wallpaper sprites inspired by
pixel-shifted wallpapers on Oakland which apparently was inspired by
Requiem's map

also adds a Low Shadow decal which sits beneath the fake platforms
(didn't want to var edit it every time). it has it's own icon instead of
just being pixel-shifted because eventually i'm going to make a bunch of
different shadow lengths etc. etc.
## Why It's Good For The Game
mappers 🔛 🔝 
## Changelog
:cl:
map: fake platforms and low shadows
/:cl:

modular_darkpack/modules/greebles/code/fake_platforms.dm
@@ -1,0 +1,108 @@
+#define FAKE_PLATFORM_HELPER(platform_type, platform_icon, double_icon)	\
+	/obj/fake_platform/##platform_type {	\
+		icon_state = ##platform_icon; \
+	}	\
+	/obj/fake_platform/##platform_type/double{	\
+		icon_state = ##double_icon;	\
+	}	\
+	/obj/fake_platform/##platform_type/easyclimb { \
+		density = FALSE; \
+		climbable = FALSE; \
+	}	\
+	/obj/fake_platform/##platform_type/double/easyclimb { \
+		density = FALSE; \
+		climbable = FALSE; \
+	}	\
+	/obj/fake_platform/##platform_type/noclimb { \
+		climbable = FALSE; \
+	}	\
+	/obj/fake_platform/##platform_type/double/noclimb { \
+		climbable = FALSE; \
+	}
+
+/obj/fake_platform
+	name = "platform"
+	desc = "Don't stub your toe."
+	icon = 'modular_darkpack/modules/greebles/icons/fake_platforms.dmi'
+	abstract_type = /obj/fake_platform
+	flags_1 = ON_BORDER_1
+	obj_flags = CAN_BE_HIT | BLOCKS_CONSTRUCTION_DIR | IGNORE_DENSITY
+	density = TRUE
+	anchored = TRUE
+	dir = NORTH
+	pass_flags_self = LETPASSTHROW|PASSSTRUCTURE
+	layer = BELOW_CATWALK_LAYER
+	plane = WALL_PLANE
+
+	var/climbable = TRUE
+
+/obj/fake_platform/Initialize(mapload)
+	. = ..()
+	if(climbable)
+		AddElement(/datum/element/climbable)
+
+	if(density && flags_1 & ON_BORDER_1) // blocks normal movement from and to the direction it's facing.
+		var/static/list/loc_connections = list(
+			COMSIG_ATOM_EXIT = PROC_REF(on_exit),
+		)
+		AddElement(/datum/element/connect_loc, loc_connections)
+
+/obj/fake_platform/CanPass(atom/movable/mover, border_dir)
+	. = ..()
+	if(border_dir & dir)
+		return . || mover.throwing || (mover.movement_type & MOVETYPES_NOT_TOUCHING_GROUND)
+	return TRUE
+
+/obj/fake_platform/CanAStarPass(to_dir, datum/can_pass_info/pass_info)
+	if(!(to_dir & dir))
+		return TRUE
+	return ..()
+
+/obj/fake_platform/proc/on_exit(datum/source, atom/movable/leaving, direction)
+	SIGNAL_HANDLER
+
+	if(leaving == src)
+		return // Let's not block ourselves.
+
+	if(!(direction & dir))
+		return
+
+	if (!density)
+		return
+
+	if (leaving.throwing)
+		return
+
+	if (leaving.movement_type & (PHASING|MOVETYPES_NOT_TOUCHING_GROUND))
+		return
+
+	if (leaving.move_force >= MOVE_FORCE_EXTREMELY_STRONG)
+		return
+
+	leaving.Bump(src)
+	return COMPONENT_ATOM_BLOCK_EXIT
+
+FAKE_PLATFORM_HELPER(wallpaper, "wallpaper", "wallpaper_double")
+FAKE_PLATFORM_HELPER(grey, "grey", "grey_double")
+FAKE_PLATFORM_HELPER(grey, "grey", "grey_double")
+FAKE_PLATFORM_HELPER(light, "light", "light_double")
+FAKE_PLATFORM_HELPER(asylum, "asylum", "asylum_double")
+FAKE_PLATFORM_HELPER(asylum_clean, "asylum_clean", "asylum_clean_double")
+FAKE_PLATFORM_HELPER(club, "club", "club_double")
+FAKE_PLATFORM_HELPER(cheap, "cheap", "cheap_double")
+FAKE_PLATFORM_HELPER(green, "green", "green_double")
+FAKE_PLATFORM_HELPER(stripe, "stripe", "stripe_double")
+FAKE_PLATFORM_HELPER(rich, "rich", "rich_double")
+FAKE_PLATFORM_HELPER(rich_alt, "rich_alt", "rich_alt_double")
+FAKE_PLATFORM_HELPER(stone, "stone", "stone_double")
+FAKE_PLATFORM_HELPER(stone_dual, "stone_dual", "stone_dual_double")
+FAKE_PLATFORM_HELPER(dgreen, "dgreen", "dgreen_double")
+FAKE_PLATFORM_HELPER(dred, "dred", "dred_double")
+FAKE_PLATFORM_HELPER(gold, "gold", "gold_double")
+FAKE_PLATFORM_HELPER(gold_alt, "gold_alt", "gold_alt_double")
+FAKE_PLATFORM_HELPER(gold_simple, "gold_simple", "gold_simple_double")
+FAKE_PLATFORM_HELPER(necro, "necro", "necro_double")
+FAKE_PLATFORM_HELPER(padded, "padded", "padded_double")
+FAKE_PLATFORM_HELPER(lightpadded, "lightpadded", "lightpadded_double")
+
+#undef FAKE_PLATFORM_HELPER

modular_darkpack/modules/decor/code/decals.dm
@@ -24,6 +24,9 @@
 		forceMove(get_step(src, NORTH))
 		pixel_y = -32
 
+/obj/effect/decal/shadow/low
+	icon_state = "shadow_low"
+
 /obj/effect/decal/support
 	name = "support"
 	icon = 'modular_darkpack/modules/decor/icons/decals.dmi'

tgstation.dme
@@ -6996,6 +6996,7 @@
 #include "modular_darkpack\modules\government\code\passport.dm"
 #include "modular_darkpack\modules\graveyard\code\graveyard.dm"
 #include "modular_darkpack\modules\graveyard\code\graveyard_area.dm"
+#include "modular_darkpack\modules\greebles\code\fake_platforms.dm"
 #include "modular_darkpack\modules\human_visuals\code\extra_human_sprites.dm"
 #include "modular_darkpack\modules\jobs\code\_departments.dm"
 #include "modular_darkpack\modules\jobs\code\_job_assignment.dm"

commit 80652e434f53f1639adfa49e9a162a602a2eaaa1

plane fix on fake platforms (#348)
## About The Pull Request
fixes issue caused by #310 
## Why It's Good For The Game
stack trace
## Changelog
:cl:
fix: fake platforms are on the right plane
/:cl:

modular_darkpack/modules/greebles/code/fake_platforms.dm
@@ -32,7 +32,6 @@
 	dir = NORTH
 	pass_flags_self = LETPASSTHROW|PASSSTRUCTURE
 	layer = BELOW_CATWALK_LAYER
-	plane = WALL_PLANE
 
 	var/climbable = TRUE
 